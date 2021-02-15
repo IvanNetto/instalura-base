d07471a077423ad1cb0097c69d22429c2c6e40d5
IvanNetto <inetto.ferreira@gmail.com>
2021-02-15 14:53:00 -0300

    fixing caseSentisive

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Logo } from '../../../Theme/Logo';
 import { Button } from '../Button';
-import { MenuWrapper } from './Styles/menuWrapper';
+import { MenuWrapper } from './Styles/MenuWrapper';
 
 export default function Menu() {
   return (
